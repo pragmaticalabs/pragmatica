7d66b5c25b58b952350bce48cff7cffe7f59f4d9
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2025-08-11 11:52:15 +0200

    chore(core): complete MessageRouter hierarchy implementation and aether integration

--- slice/src/main/java/org/pragmatica/aether/cluster/NodeDeploymentManager.java
+++ slice/src/main/java/org/pragmatica/aether/cluster/NodeDeploymentManager.java
@@ -12,19 +12,19 @@ import java.util.regex.Pattern;
 
 public interface NodeDeploymentManager {
     sealed interface NodeDeploymentState {
-        default <K extends CharSequence,V> void onValuePut(ValuePut<K,V> valuePut) {}
-        default <K extends CharSequence,V> void onValueRemove(ValueRemove<K, V> valueRemove) {}
+        default void onValuePut(ValuePut<?,?> valuePut) {}
+        default void onValueRemove(ValueRemove<?,?> valueRemove) {}
 
         record DormantNodeDeploymentState() implements NodeDeploymentState { }
         record ActiveNodeDeploymentState(Pattern pattern, NodeId self) implements NodeDeploymentState {
 
             @Override
-            public <K extends CharSequence, V>  void onValuePut(ValuePut<K,V> valuePut) {
+            public void onValuePut(ValuePut<?,?> valuePut) {
 
             }
 
             @Override
-            public <K extends CharSequence,V> void onValueRemove(ValueRemove<K, V> valueRemove) {
+            public void onValueRemove(ValueRemove<?,?> valueRemove) {
 
             }
         }
@@ -33,11 +33,11 @@ public interface NodeDeploymentManager {
     static NodeDeploymentManager nodeSliceManager(NodeId self, MessageRouter router, SliceStore sliceStore) {
         record deploymentManager(NodeId self, SliceStore sliceManager, AtomicReference<NodeDeploymentState> state) implements NodeDeploymentManager {
 
-             public <K extends CharSequence,V> void onValuePut(ValuePut<K,V> valuePut) {
+             public void onValuePut(ValuePut<?,?> valuePut) {
                 state.get().onValuePut(valuePut);
             }
 
-            public <K extends CharSequence,V> void onValueRemove(ValueRemove<K, V> valueRemove) {
+            public void onValueRemove(ValueRemove<?,?> valueRemove) {
                 state.get().onValueRemove(valueRemove);
             }
 
@@ -47,13 +47,19 @@ public interface NodeDeploymentManager {
                     case DISAPPEARED -> state().set(new NodeDeploymentState.DormantNodeDeploymentState());
                 }
             }
+            
+            private Pattern buildPattern() {
+                // Create pattern based on node ID for slice deployment matching
+                return Pattern.compile("slice-" + self.id() + "-.*");
+            }
         }
 
         var deploymentManager = new deploymentManager(self, sliceStore, new AtomicReference<>(new NodeDeploymentState.DormantNodeDeploymentState()));
 
-        router.addRoute(ValuePut.class, deploymentManager::onValuePut);
-        router.addRoute(ValueRemove.class, deploymentManager::onValueRemove);
-        router.addRoute(QuorumStateNotification.class, deploymentManager::onQuorumStateChange);
+        var mutableRouter = (MessageRouter.MutableRouter) router;
+        mutableRouter.addRoute(ValuePut.class, deploymentManager::onValuePut);
+        mutableRouter.addRoute(ValueRemove.class, deploymentManager::onValueRemove);
+        mutableRouter.addRoute(QuorumStateNotification.class, deploymentManager::onQuorumStateChange);
 
         return deploymentManager;
     }
