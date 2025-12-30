4f957b6ee118d258d3d2f7682ea5308b9998d3b4
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2025-12-30 13:47:58 +0100

    refactor: use MultiThreadIoEventLoopGroup in http-server

--- http-server/src/main/java/org/pragmatica/http/server/impl/NettyHttpServer.java
+++ http-server/src/main/java/org/pragmatica/http/server/impl/NettyHttpServer.java
@@ -1,16 +1,5 @@
 package org.pragmatica.http.server.impl;
 
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpServerCodec;
-import io.netty.handler.ssl.SslContext;
 import org.pragmatica.http.server.HttpServer;
 import org.pragmatica.http.server.HttpServerConfig;
 import org.pragmatica.http.server.HttpServerError;
@@ -20,10 +9,23 @@ import org.pragmatica.lang.Option;
 import org.pragmatica.lang.Promise;
 import org.pragmatica.lang.Result;
 import org.pragmatica.lang.Unit;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.function.BiConsumer;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.MultiThreadIoEventLoopGroup;
+import io.netty.channel.nio.NioIoHandler;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.ssl.SslContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.pragmatica.lang.Unit.unit;
 
@@ -41,11 +43,9 @@ public final class NettyHttpServer implements HttpServer {
     private EventLoopGroup workerGroup;
     private Channel serverChannel;
 
-    private NettyHttpServer(
-            HttpServerConfig config,
-            BiConsumer<RequestContext, ResponseWriter> handler,
-            Option<SslContext> sslContext
-    ) {
+    private NettyHttpServer(HttpServerConfig config,
+                            BiConsumer<RequestContext, ResponseWriter> handler,
+                            Option<SslContext> sslContext) {
         this.config = config;
         this.handler = handler;
         this.sslContext = sslContext;
@@ -58,83 +58,76 @@ public final class NettyHttpServer implements HttpServer {
      * @param handler request handler
      * @return server instance or error
      */
-    public static Promise<HttpServer> create(
-            HttpServerConfig config,
-            BiConsumer<RequestContext, ResponseWriter> handler
-    ) {
+    public static Promise<HttpServer> create(HttpServerConfig config,
+                                             BiConsumer<RequestContext, ResponseWriter> handler) {
         Result<Option<SslContext>> sslResult = config.tls()
-                .map(TlsContextFactory::create)
-                .map(result -> result.map(Option::some))
-                .fold(
-                        () -> Result.success(Option.empty()),
-                        result -> result
-                );
-
-        return sslResult
-                .map(ssl -> (HttpServer) new NettyHttpServer(config, handler, ssl))
-                .async();
+                                                     .map(TlsContextFactory::create)
+                                                     .map(result -> result.map(Option::some))
+                                                     .fold(() -> Result.success(Option.empty()),
+                                                           result -> result);
+        return sslResult.map(ssl -> (HttpServer) new NettyHttpServer(config, handler, ssl))
+                        .async();
     }
 
     @Override
     public Promise<Unit> start() {
         return Promise.promise(promise -> {
-            bossGroup = new NioEventLoopGroup(1);
-            workerGroup = new NioEventLoopGroup();
-
-            try {
-                var bootstrap = new ServerBootstrap()
-                        .group(bossGroup, workerGroup)
-                        .channel(NioServerSocketChannel.class)
-                        .childHandler(new ChannelInitializer<SocketChannel>() {
-                            @Override
-                            protected void initChannel(SocketChannel ch) {
-                                var pipeline = ch.pipeline();
-
-                                sslContext.onPresent(ssl -> pipeline.addLast(ssl.newHandler(ch.alloc())));
-
-                                pipeline.addLast(new HttpServerCodec());
-                                pipeline.addLast(new HttpObjectAggregator(config.maxContentLength()));
-                                pipeline.addLast(new HttpRequestHandler(handler));
-                            }
-                        })
-                        .option(ChannelOption.SO_BACKLOG, 128)
-                        .childOption(ChannelOption.SO_KEEPALIVE, true);
-
-                var channelFuture = bootstrap.bind(config.port());
-
-                channelFuture.addListener(future -> {
-                    if (future.isSuccess()) {
-                        serverChannel = channelFuture.channel();
-                        var protocol = sslContext.isPresent() ? "HTTPS" : "HTTP";
-                        LOG.info("{} server started on port {}", protocol, config.port());
-                        promise.succeed(unit());
-                    } else {
-                        cleanup();
-                        promise.fail(new HttpServerError.BindFailed(config.port(), future.cause()));
-                    }
-                });
-            } catch (Exception e) {
-                cleanup();
-                promise.fail(new HttpServerError.BindFailed(config.port(), e));
-            }
-        });
+                                   bossGroup = new MultiThreadIoEventLoopGroup(1, NioIoHandler.newFactory());
+                                   workerGroup = new MultiThreadIoEventLoopGroup(NioIoHandler.newFactory());
+                                   try{
+                                   var bootstrap = new ServerBootstrap().group(bossGroup, workerGroup)
+                                                   .channel(NioServerSocketChannel.class)
+                                                   .childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel ch) {
+                                                                     var pipeline = ch.pipeline();
+                                                                     sslContext.onPresent(ssl -> pipeline.addLast(ssl.newHandler(ch.alloc())));
+                                                                     pipeline.addLast(new HttpServerCodec());
+                                                                     pipeline.addLast(new HttpObjectAggregator(config.maxContentLength()));
+                                                                     pipeline.addLast(new HttpRequestHandler(handler));
+                                                                 }
+        })
+                                                   .option(ChannelOption.SO_BACKLOG, 128)
+                                                   .childOption(ChannelOption.SO_KEEPALIVE, true);
+                                   var channelFuture = bootstrap.bind(config.port());
+                                   channelFuture.addListener(future -> {
+                                                                 if (future.isSuccess()) {
+                                                                 serverChannel = channelFuture.channel();
+                                                                 var protocol = sslContext.isPresent()
+                                                                                ? "HTTPS"
+                                                                                : "HTTP";
+                                                                 LOG.info("{} server started on port {}",
+                                                                          protocol,
+                                                                          config.port());
+                                                                 promise.succeed(unit());
+                                                             }else {
+                                                                 cleanup();
+                                                                 promise.fail(new HttpServerError.BindFailed(config.port(),
+                                                                                                             future.cause()));
+                                                             }
+                                                             });
+                               } catch (Exception e) {
+                                   cleanup();
+                                   promise.fail(new HttpServerError.BindFailed(config.port(), e));
+                               }
+                               });
     }
 
     @Override
     public Promise<Unit> stop() {
         return Promise.promise(promise -> {
-            LOG.info("Stopping HTTP server on port {}", config.port());
-
-            if (serverChannel != null) {
-                serverChannel.close().addListener(future -> {
-                    cleanup();
-                    promise.succeed(unit());
-                });
-            } else {
-                cleanup();
-                promise.succeed(unit());
-            }
-        });
+                                   LOG.info("Stopping HTTP server on port {}", config.port());
+                                   if (serverChannel != null) {
+                                   serverChannel.close()
+                                                .addListener(future -> {
+                                   cleanup();
+                                   promise.succeed(unit());
+                               });
+                               }else {
+                                   cleanup();
+                                   promise.succeed(unit());
+                               }
+                               });
     }
 
     @Override
