ed5158c0fd4a501d3d56afb50ef82ca7761ccadf
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2026-05-19 05:27:53 +0200

    fix(aether-control): StubMetricsCollector implements emitPeriodicConnectivity

--- aether/aether-control/src/test/java/org/pragmatica/aether/controller/fsm/ControlLoopFsmTest.java
+++ aether/aether-control/src/test/java/org/pragmatica/aether/controller/fsm/ControlLoopFsmTest.java
@@ -47,6 +47,7 @@ import org.pragmatica.statemachine.FsmTestHarness;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -323,5 +324,6 @@ class ControlLoopFsmTest {
         @Override public Option<AggregatedReachabilitySnapshot> lastReachabilitySnapshot() { return Option.none(); }
         @Override public void setLocalSnapshotSupplier(Supplier<Option<AggregatedReachabilitySnapshot>> supplier) {}
         @Override public Option<AggregatedReachabilitySnapshot> bestSnapshot() { return Option.none(); }
+        @Override public void emitPeriodicConnectivity(Set<NodeId> topology, Set<NodeId> connected, NodeId self, long nowMs) {}
     }
 }
